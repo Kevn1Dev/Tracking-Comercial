47ac1d32a9d48938f386d0a9287046d88ca9b6ed
Kevn1Dev <kevinnicolas302@gmail.com>
2024-11-12 03:55:30 -0300

    Create TrackingcomercialApplicationTests.java

---
+++ trackingcomercial/src/test/java/com/trackingcomercial/trackingcomercial/TrackingcomercialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackingcomercial.trackingcomercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingcomercialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
