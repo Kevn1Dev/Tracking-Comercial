7bf89958725138ae542c50609464e1e55d38a4a4
Kevn1Dev <kevinnicolas302@gmail.com>
2024-11-12 03:55:21 -0300

    Create TrackingcomercialApplication.java

---
+++ trackingcomercial/src/main/java/com/trackingcomercial/TrackingcomercialApplication.java
@@ -0,0 +1,13 @@
+package com.trackingcomercial;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingcomercialApplication {
+
+    public static void main(String[] args) {
+        
+        
+    }
+
+}
