57bb2954b60f20c7988f976f3c6ed1fc1cf4d164
Kevn1Dev <kevinnicolas302@gmail.com>
2024-11-13 11:22:46 -0300

    Update ProyectoService.java

--- trackingcomercial/src/main/java/com/trackingcomercial/service/ProyectoService.java
+++ trackingcomercial/src/main/java/com/trackingcomercial/service/ProyectoService.java
@@ -3,7 +3,6 @@ package com.trackingcomercial.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Optional;
 
 import com.trackingcomercial.entity.Proyecto;
 import com.trackingcomercial.repository.ProyectoRepository;
@@ -18,8 +17,9 @@ public class ProyectoService {
         return proyectoRepository.findAll();
     }
 
-    public Optional<Proyecto> obtenerProyectoPorId(Long Id){
-        return proyectoRepository.findById(Id);
+    public Proyecto obtenerProyectoPorId(Long Id){
+        return proyectoRepository.findById(Id)
+                .orElseThrow(() -> new ResourceNotFoundException("Proyecto no Encontrado"));
     }
 
     public Proyecto guardar(Proyecto proyecto){
