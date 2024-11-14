8b096b5606f63cfa762052377dc75472c59f11b9
Kevn1Dev <kevinnicolas302@gmail.com>
2024-11-14 04:38:13 -0300

    Create navbar-componente.component.ts

---
+++ frontend/src/app/navbar-componente/navbar-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar-componente.component.html',
+  styleUrl: './navbar-componente.component.css'
+})
+export class NavbarComponenteComponent {
+
+}
