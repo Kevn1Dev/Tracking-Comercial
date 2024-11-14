315090b706244c300149768c1baa4cab6d826309
Kevn1Dev <kevinnicolas302@gmail.com>
2024-11-14 04:38:34 -0300

    Create proyectos-aprobados-componente.component.ts

---
+++ frontend/src/app/proyectos-aprobados-componente/proyectos-aprobados-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proyectos-aprobados-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './proyectos-aprobados-componente.component.html',
+  styleUrl: './proyectos-aprobados-componente.component.css'
+})
+export class ProyectosAprobadosComponenteComponent {
+
+}
