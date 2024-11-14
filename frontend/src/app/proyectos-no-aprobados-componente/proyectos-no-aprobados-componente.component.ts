2766d2e6e828a97afc23fc573f8ee74cb0056a67
Kevn1Dev <kevinnicolas302@gmail.com>
2024-11-14 04:38:58 -0300

    Create proyectos-no-aprobados-componente.component.ts

---
+++ frontend/src/app/proyectos-no-aprobados-componente/proyectos-no-aprobados-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proyectos-no-aprobados-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './proyectos-no-aprobados-componente.component.html',
+  styleUrl: './proyectos-no-aprobados-componente.component.css'
+})
+export class ProyectosNoAprobadosComponenteComponent {
+
+}
