d00af5cd777743f90f58b4a9e060de3c119714d5
Kevn1Dev <kevinnicolas302@gmail.com>
2024-11-14 04:39:03 -0300

    Create register-componente.component.ts

---
+++ frontend/src/app/register-componente/register-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-componente.component.html',
+  styleUrl: './register-componente.component.css'
+})
+export class RegisterComponenteComponent {
+
+}
