0c8d77f932db480f55dcf2184e592235f6dfdec6
Kevn1Dev <kevinnicolas302@gmail.com>
2024-11-14 04:38:45 -0300

    Create proyectos-en-espera-componente.component.ts

---
+++ frontend/src/app/proyectos-en-espera-componente/proyectos-en-espera-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proyectos-en-espera-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './proyectos-en-espera-componente.component.html',
+  styleUrl: './proyectos-en-espera-componente.component.css'
+})
+export class ProyectosEnEsperaComponenteComponent {
+
+}
