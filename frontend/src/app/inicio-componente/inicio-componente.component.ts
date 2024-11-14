7cfcd667d7502c5b6d859e150988ed2ca67f3469
Kevn1Dev <kevinnicolas302@gmail.com>
2024-11-14 04:38:54 -0300

    Create inicio-componente.component.ts

---
+++ frontend/src/app/inicio-componente/inicio-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './inicio-componente.component.html',
+  styleUrl: './inicio-componente.component.css'
+})
+export class InicioComponenteComponent {
+
+}
