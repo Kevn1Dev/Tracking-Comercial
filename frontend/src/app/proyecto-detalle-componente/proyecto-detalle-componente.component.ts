70194dabea0dc6733a2fa263633693842fe09e6e
Kevn1Dev <kevinnicolas302@gmail.com>
2024-11-14 04:38:24 -0300

    Create proyecto-detalle-componente.component.ts

---
+++ frontend/src/app/proyecto-detalle-componente/proyecto-detalle-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proyecto-detalle-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './proyecto-detalle-componente.component.html',
+  styleUrl: './proyecto-detalle-componente.component.css'
+})
+export class ProyectoDetalleComponenteComponent {
+
+}
