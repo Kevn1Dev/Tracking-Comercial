68db889b6dc6fae980f8087ff2d29b05dc705316
Kevn1Dev <kevinnicolas302@gmail.com>
2024-11-14 04:39:15 -0300

    Create aside-componente.component.ts

---
+++ frontend/src/app/aside-componente/aside-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aside-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './aside-componente.component.html',
+  styleUrl: './aside-componente.component.css'
+})
+export class AsideComponenteComponent {
+
+}
